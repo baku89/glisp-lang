3ce5ff24a87c97c1535f846f6a8bf089dddf4b9c
Baku Hashimoto <mail@baku89.com>
2023-03-08 22:56:49 +0900

    Refactor nullishEqual

--- src/util/nullishEqual.ts
+++ src/util/nullishEqual.ts
@@ -1,10 +1,22 @@
+/**
+ * Evaluates the equality of two values when both are not nullish. Also returns true if both are nullish
+ */
 export function nullishEqual<T>(
 	a: T | null | undefined,
 	b: T | null | undefined,
-	f: (x: T, y: T) => boolean
+	isEqual: (x: T, y: T) => boolean
 ): boolean {
-	if (a !== null && a !== undefined && b !== null && b !== undefined)
-		return f(a, b)
-	if (!a && !b) return true
+	const isANullish = a === null || a === undefined
+	const isBNullish = b === null || b === undefined
+
+	if (!isANullish && !isBNullish) {
+		// If both are not nullish values, then evaluate the equality
+		return isEqual(a, b)
+	}
+	if (isANullish && isBNullish) {
+		// If both are nullish, return true
+		return true
+	}
+	// Otherwise, it means that only either one is nullish
 	return false
 }
