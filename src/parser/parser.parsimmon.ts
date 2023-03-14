384c84b28b21c0b431290624d5d493001c01eda3
Baku Hashimoto <mail@baku89.com>
2023-03-14 11:15:34 +0900

    Impl parsing Literals, App with Parsimmon

---
+++ src/parser/parser.parsimmon.ts
@@ -0,0 +1,77 @@
+import P from 'parsimmon'
+
+import {App, Expr, NumberLiteral, StringLiteral} from '../expr'
+
+function zip<T1, T2, T3, T4>(
+	coll: [T1, T2, T3?, T4?][]
+): [T1[], T2[], T3[], T4[]] {
+	const as: T1[] = []
+	const bs: T2[] = []
+	const cs: T3[] = []
+	const ds: T4[] = []
+
+	for (const [a, b, c, d] of coll) {
+		as.push(a)
+		bs.push(b)
+		if (c) cs.push(c)
+		if (d) ds.push(d)
+	}
+
+	return [as, bs, cs, ds]
+}
+
+interface IParser {
+	Program: Expr
+	Expr: Expr
+	NumberLiteral: NumberLiteral
+	StringLiteral: StringLiteral
+	App: App
+}
+
+export const Parser = P.createLanguage<IParser>({
+	Program(r) {
+		return P.seqMap(P.optWhitespace, r.Expr, P.optWhitespace, (_, expr) => {
+			return expr
+		}).desc('program')
+	},
+	Expr(r) {
+		return P.alt(r.NumberLiteral, r.StringLiteral, r.App).desc('expression')
+	},
+	NumberLiteral() {
+		return P.seq(
+			P.regex(/[+-]?/),
+			P.seq(P.digits, P.string('.')).tie().fallback(''),
+			P.digit.atLeast(1).tie()
+		)
+			.tie()
+			.map(raw => {
+				const expr = new NumberLiteral(parseFloat(raw))
+				expr.extras = {raw}
+				return expr
+			})
+			.desc('numeric literal')
+	},
+	StringLiteral() {
+		return P.string('"')
+			.then(P.noneOf('"').many().tie())
+			.skip(P.string('"'))
+			.map(raw => new StringLiteral(raw))
+			.desc('string literal')
+	},
+	App(r) {
+		return P.string('(')
+			.then(P.seq(P.optWhitespace, P.seq(r.Expr, P.optWhitespace).many()))
+			.skip(P.string(')'))
+			.map(([d0, itemDs]) => {
+				const [items, ds] = zip(itemDs)
+
+				const delimiters = [d0, ...ds]
+
+				const expr = new App(...items)
+				expr.extras = {delimiters}
+
+				return expr
+			})
+			.desc('function application')
+	},
+})
