b6de43d484031ca2cda160434f3c23d48ceabdfc
Baku Hashimoto <mail@baku89.com>
2023-03-15 18:36:14 +0900

    Organize the exports of root

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
-import * as Expr from './expr'
+export * as Expr from './expr'
+export * as Value from './value'
+
+import {InnerNode} from './expr'
 import {Parser} from './parser'
 import {PreludeScope} from './std/prelude'
-import * as Value from './value'
 
-export {Expr, Value, PreludeScope}
+export {withLog} from './log'
+export type {WithLog, Log} from './log'
 
-export function parse(input: string) {
+export {PreludeScope}
+
+export function tryParse(input: string, outer: InnerNode = PreludeScope) {
 	const expr = Parser.Program.tryParse(input)
-	expr.parent = PreludeScope
+	expr.parent = outer
 	return expr
+}
+
+export function parse(input: string, outer: InnerNode = PreludeScope) {
+	const result = Parser.Program.parse(input)
+	if (result.status) {
+		result.value.parent = outer
+	}
+	return result
 }
