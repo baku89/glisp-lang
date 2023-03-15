04fa5748559457109d9b2afd0454849e1d6c62ab
Baku Hashimoto <mail@baku89.com>
2023-03-15 23:22:07 +0900

    Set path alias

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import {defineConfig} from 'vite'
+import vitePluginStylusAlias from 'vite-plugin-stylus-alias'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+	plugins: [vue(), vitePluginStylusAlias.default()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			glisp: path.resolve(__dirname, '..'),
+		},
+	},
 })
