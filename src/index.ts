4a5b61007d19a6668fa84bf8a1ed3e5ceb93bcc9
Baku Hashimoto <mail@baku89.com>
2023-03-15 13:49:12 +0900

    Set Prelude as a parent when parsing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import * as Expr from './expr'
-import {parse} from './parser'
+import {Parser} from './parser'
 import {PreludeScope} from './std/prelude'
 import * as Value from './value'
 
-export {Expr, Value, PreludeScope, parse}
+export {Expr, Value, PreludeScope}
+
+export function parse(input: string) {
+	const expr = Parser.Program.tryParse(input)
+	expr.parent = PreludeScope
+	return expr
+}
