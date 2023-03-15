cad590df11c43b4a7cd10fbcb927e644701fd4bd
Baku Hashimoto <mail@baku89.com>
2023-03-15 15:20:15 +0900

    Update theme

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -3,8 +3,10 @@ import {ref} from 'vue'
 import {parse} from '../..'
 
 const result = ref(
-	parse('[(++ "Hello" " World") (+ 1 2)]').eval().result.print()
+	parse('[(++ "Hello" " (+) " "World!") (+ 3.2 2)]').eval().result.print()
 )
+
+const inputs = ref()
 
 defineExpose({
 	result,
@@ -12,12 +14,45 @@ defineExpose({
 </script>
 
 <template>
-	<h1>Glisp / REPL</h1>
-	<hr />
-	<p>
-		A Lisp-based Design Tool Bridging Graphic Design and Computational Arts.
-	</p>
-	<code>{{ result }}</code>
+	<header class="gheader"></header>
+	<main>
+		<h1>Glisp / REPL</h1>
+		<hr />
+		<p>
+			<a href="https://scrapbox.io/glisp/Glisp_v2:_Documentation"
+				>Language Specification</a
+			>
+			(in 🇯🇵)
+		</p>
+		<p>
+			<em>Glisp</em> (/dʒiːlisp/ /glisp/) is a Lisp-based design environment
+			that introduces the approach of computational arts to the world of
+			repetitive and manual design. Mainly developed by Baku Hashimoto (橋本麦).
+		</p>
+		<code class="repl">{{ result }}</code>
+	</main>
 </template>
 
-<style scoped></style>
+<style scoped lang="stylus">
+
+.gheader
+	position absolute
+	top 0
+	left -1px
+	right -1px
+	border-color var(--color-on-surface)
+	border-width 0 1px 1px 1px
+	background var(--color-surface)
+	height 3rem
+	border-radius 0 0 1em 1.5em
+
+main
+	max-width 50rem
+.repl
+	display block
+	padding 1em
+	border-radius 1em
+	border 1px solid var(--color-on-surface)
+	background var(--color-surface)
+	color var(--color-on-surface)
+</style>
