27a11fee8662dc48291e3209e2b9f557e1c65c4a
Baku Hashimoto <mail@baku89.com>
2023-04-07 09:27:31 +0900

    Impl createFoldFn2

--- src/value/walk.ts
+++ src/value/walk.ts
@@ -82,3 +82,69 @@ export function createFoldFn<U>(
 		}
 	}
 }
+
+type Visitors2<U> = {
+	[T in ValueType]?: (value: TypeToValue<T>) => U
+}
+
+export function createFoldFn2<U>(
+	visitors: Visitors2<U>,
+	defaultFn: (value: Val.Value) => U,
+	concat: (...xs: U[]) => U
+) {
+	return function fold(value: Val.Value): U {
+		const type = value.type
+
+		switch (type) {
+			case 'All':
+				return (visitors[type] ?? defaultFn)(value)
+			case 'Never':
+				return (visitors[type] ?? defaultFn)(value)
+			case 'Unit':
+				return (visitors[type] ?? defaultFn)(value)
+			case 'PrimType':
+				return (visitors[type] ?? defaultFn)(value)
+			case 'Prim':
+				return (visitors[type] ?? defaultFn)(value)
+			case 'EnumType':
+				return (visitors[type] ?? defaultFn)(value)
+			case 'Enum':
+				return (visitors[type] ?? defaultFn)(value)
+			case 'FnType': {
+				const f = visitors[type] ?? defaultFn
+
+				const us = [f(value), ...values(value.params).map(fold)]
+				if (value.rest) us.push(fold(value.rest.value))
+				us.push(fold(value.ret))
+
+				return concat(...us)
+			}
+			case 'Fn': {
+				const f = visitors[type] ?? defaultFn
+				return concat(f(value), fold(value.fnType))
+			}
+			case 'Vec': {
+				const f = visitors[type] ?? defaultFn
+
+				const us = [f(value), ...value.items.map(fold)]
+				if (value.rest) us.push(fold(value))
+
+				return concat(...us)
+			}
+			case 'Dict': {
+				const f = visitors[type] ?? defaultFn
+
+				const us = [f(value), ...values(value.items).map(fold)]
+				if (value.rest) us.push(fold(value.rest))
+
+				return concat(...us)
+			}
+			case 'TypeVar':
+				return (visitors[type] ?? defaultFn)(value)
+			case 'UnionType': {
+				const f = visitors[type] ?? defaultFn
+				return concat(f(value), ...value.types.map(fold))
+			}
+		}
+	}
+}
