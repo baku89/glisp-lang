44432a06d4a1b5b420d213c8747f9e3643ae8557
Baku Hashimoto <mail@baku89.com>
2023-04-07 16:22:14 +0900

    Refactor parameter type of union

--- src/util/SetOperation.ts
+++ src/util/SetOperation.ts
@@ -1,4 +1,4 @@
-export function union<T>(...sets: Set<T>[]): Set<T> {
+export function union<T>(...sets: (Set<T> | T[])[]): Set<T> {
 	const arr = sets.flatMap(s => Array.from(s))
 	return new Set(arr)
 }
